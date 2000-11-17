9dd07520e7ece79e823db88be2e5f2a3571c8673
Jim Davidson <jgdavidson@aol.com>
2000-11-17 16:52:15 +0000

    Added Ns_PoolEnum to get at pool stats as with mutexes and threads, removing the old Ns_PoolStats API's and updating the "ns_info pools" command

--- include/nsthread.h
+++ include/nsthread.h
@@ -218,6 +218,7 @@ typedef struct Ns_MutexInfo {
 
 typedef void (Ns_ThreadInfoProc) (Ns_ThreadInfo *infoPtr, void *arg);
 typedef void (Ns_MutexInfoProc) (Ns_MutexInfo *infoPtr, void *arg);
+typedef void (Ns_PoolInfoProc) (Ns_PoolInfo *infoPtr, void *arg);
 
 /*
  * compat.c:
@@ -327,8 +328,7 @@ NS_EXTERN void *Ns_PoolRealloc(Ns_Pool *pool, void *ptr, size_t size);
 NS_EXTERN void *Ns_PoolCalloc(Ns_Pool *pool, size_t elsize, size_t nelem);
 NS_EXTERN char *Ns_PoolStrDup(Ns_Pool *pool, char *old);
 NS_EXTERN char *Ns_PoolStrCopy(Ns_Pool *pool, char *old);
-NS_EXTERN Ns_PoolInfo *Ns_PoolStats(Ns_Pool *pool);
-NS_EXTERN Ns_Pool *Ns_ThreadPool(void);
+NS_EXTERN void Ns_PoolEnum(Ns_PoolInfoProc *proc, void *arg);
 NS_EXTERN void *Ns_ThreadMalloc(size_t size);
 NS_EXTERN void *Ns_ThreadAlloc(size_t size);
 NS_EXTERN void *Ns_ThreadRealloc(void *ptr, size_t size);
@@ -336,7 +336,6 @@ NS_EXTERN void Ns_ThreadFree(void *ptr);
 NS_EXTERN void *Ns_ThreadCalloc(size_t nelem, size_t elsize);
 NS_EXTERN char *Ns_ThreadStrDup(char *old);
 NS_EXTERN char *Ns_ThreadStrCopy(char *old);
-NS_EXTERN Ns_PoolInfo *Ns_ThreadPoolStats(Ns_Thread *thread);
 
 /*
  * pthread.c, sproc.c, win32.c:
