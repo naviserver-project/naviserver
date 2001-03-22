435738103ca999833042aff45b5d0a146eb520da
Jim Davidson <jgdavidson@aol.com>
2001-03-22 21:39:27 +0000

    Silenced compiler warning

--- nsd/uuencode.c
+++ nsd/uuencode.c
@@ -136,7 +136,7 @@ Ns_HtuuEncode(unsigned char *input, unsigned int len, char *output)
 	*q++ = '=';
     }
     *q = '\0';
-    return (q - output);
+    return (q - (unsigned char *) output);
 }
 
 
@@ -181,7 +181,7 @@ Ns_HtuuDecode(char *input, unsigned char *output, int outputlen)
     while (pr2six[(int)(*p)] >= 0) {
 	++p;
     }
-    len = p - input;
+    len = p - (unsigned char *) input;
 
     /*
      * Decode every four input bytes.
