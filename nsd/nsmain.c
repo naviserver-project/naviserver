540665564b57dd611048c6268c7b81a91a53803b
Jim Davidson <jgdavidson@aol.com>
2001-04-24 19:55:33 +0100

    Removed unused variables

--- nsd/nsmain.c
+++ nsd/nsmain.c
@@ -82,7 +82,7 @@ static char *FindConfig(char *config);
 int
 Ns_Main(int argc, char **argv, Ns_ServerInitProc *initProc)
 {
-    int            i, fd, nargs;
+    int            i, fd;
     char          *config;
     Ns_Time 	   timeout;
     char	   cwd[PATH_MAX];
@@ -175,7 +175,6 @@ Ns_Main(int argc, char **argv, Ns_ServerInitProc *initProc)
 
     opterr = 0;
     while ((i = getopt(argc, argv, "qhpzifVl:s:t:" POPTS)) != -1) {
-	++nargs;
         switch (i) {
 	case 'l':
 	    sprintf(cwd, "TCL_LIBRARY=%s", optarg);
