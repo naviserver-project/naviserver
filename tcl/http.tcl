689620ed17e4d4e99a3683301d4e3b396ce5d954
Scott S. Goodwin <scott@scottg.net>
2001-07-17 04:40:46 +0100

    You can now 'ns_httpopen GET /index.html', http://hostname:port/ will be prepended

--- tcl/http.tcl
+++ tcl/http.tcl
@@ -54,6 +54,19 @@
 
 proc ns_httpopen {method url {rqset ""} {timeout 30} {pdata ""}} {
     #
+    # Determine if url is local; prepend site address if so
+    #
+
+    if [string match /* $url] {
+        set host "http://[ns_config ns/server/[ns_info server]/module/nssock hostname]"
+        set port [ns_config ns/server/[ns_info server]/module/nssock port]             
+        if { $port != 80 } {
+            append host ":$port"
+        }
+        set url "$host$url"
+    }
+
+    #
     # Verify that the URL is an HTTP url.
     #
     
