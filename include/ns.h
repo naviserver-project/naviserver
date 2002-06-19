69cf28adb8fd63743717050a4a0be60c995c83be
Jim Davidson <jgdavidson@aol.com>
2002-06-19 11:18:52 +0100

    Added Ns_TclCreateInterp()

--- include/ns.h
+++ include/ns.h
@@ -956,6 +956,7 @@ NS_EXTERN int Ns_TclGetOpenFd(Tcl_Interp *interp, char *chanId, int write,
 NS_EXTERN int Ns_TclInitInterps(char *server, Ns_TclInterpInitProc *proc, void *arg);
 NS_EXTERN void Ns_TclRegisterDeferred(Tcl_Interp *interp, Ns_TclDeferProc *proc, void *arg);
 NS_EXTERN void Ns_TclMarkForDelete(Tcl_Interp *interp);
+NS_EXTERN Tcl_Interp *Ns_TclCreateInterp(void);
 NS_EXTERN Tcl_Interp *Ns_TclAllocateInterp(char *server);
 NS_EXTERN void Ns_TclDeAllocateInterp(Tcl_Interp *interp);
 NS_EXTERN char *Ns_TclLibrary(char *server);
