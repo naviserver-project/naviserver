9e4cb0373348f3998a6d5e0f468f48251ba4bc73
Jim Davidson <jgdavidson@aol.com>
2002-07-15 00:10:19 +0100

    ns_sleep command now understands microsecond resolution timespecs from ns_time

--- nsd/tclmisc.c
+++ nsd/tclmisc.c
@@ -488,25 +488,25 @@ NsTclSleepCmd(ClientData dummy, Tcl_Interp *interp, int argc, char **argv)
 int
 NsTclSleepObjCmd(ClientData dummy, Tcl_Interp *interp, int objc, Tcl_Obj **objv)
 {
-    int seconds;
+    Ns_Time time;
+    struct timeval tv;
 
     if (objc != 2) {
-        Tcl_WrongNumArgs(interp, 1, objv, "seconds");
+        Tcl_WrongNumArgs(interp, 1, objv, "timespec");
         return TCL_ERROR;
     }
-
-    if (Tcl_GetIntFromObj(interp, objv[1], &seconds) != TCL_OK) {
+    if (Ns_TclGetTimeFromObj(interp, objv[1], &time) != TCL_OK) {
         return TCL_ERROR;
     }
-
-    if (seconds < 0) {
-        Tcl_AppendStringsToObj(Tcl_GetObjResult(interp), "invalid seconds \"", 
-            Tcl_GetString(objv[1]),
-	        "\": should be >= 0", NULL);
+    Ns_AdjTime(&time);
+    if (time.sec < 0 || (time.sec == 0 && time.usec < 0)) {
+        Tcl_AppendResult(interp, "invalid timespec: ", 
+            Tcl_GetString(objv[1]), NULL);
         return TCL_ERROR;
     }
-
-    sleep(seconds);
+    tv.tv_sec = time.sec;
+    tv.tv_usec = time.usec;
+    (void) select(0, NULL, NULL, NULL, &tv);
     return TCL_OK;
 }
 
