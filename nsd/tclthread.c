8f6c3bc359f0f0ba9daa420a4aba520ad3ec6ae2
Zoran Vasiljevic <zv@archiware.com>
2003-05-14 21:13:30 +0100

    Fixed compatibility to the 3.x ns_event command
    Fixed NsTclCondObjCmd to be compatible to the 3.x pendant in
    way it threats the optional timeout argument. The 3.x reverted
    to indefinite (i.e. non-timewait) condvar waits when the
    optional "timeout" argument was given as zero.
    
    The 4.x version just exited with NS_TIMEOUT (= 0) in such
    cases breaking Tcl scripts written for 3.x.
    
    The corrective measure is to check the passed timeout value
    and if == 0, revert to non-timewait condition waits as the
    3.x does (did).

--- nsd/tclthread.c
+++ nsd/tclthread.c
@@ -328,7 +328,7 @@ NsTclCondObjCmd(ClientData data, Tcl_Interp *interp, int objc,
 	    Ns_Event *eventPtr = (Ns_Event *) condPtr;
             result = Ns_TimedWaitForEvent(eventPtr, lock, timeout.sec);
 	} else {
-	    if (objc < 5) {
+	    if (objc < 5 || (timeout.sec == 0 && timeout.usec == 0)) {
 		Ns_CondWait(condPtr, lock);
 		result = NS_OK;
 	    } else {
