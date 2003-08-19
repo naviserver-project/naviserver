729f4428baeff07209b9f7efdf51fad704b1e020
Nathan Folkman <nathan.folkman@gmail.com>
2003-08-19 22:23:08 +0100

    Add HTTP status codes 100, 101, 203, 207, 300, 307, 402, 416, 417, 423, 507
    RFC 2616 (Hypertext Transfer Protocol -- HTTP/1.1)
    RFC 2518 (HTTP Extensions for Distributed Authoring -- WEBDAV)

--- nsd/return.c
+++ nsd/return.c
@@ -58,19 +58,26 @@ static struct {
     int	  status;
     char *reason;
 } reasons[] = {
+    {100, "Continue"},
+    {101, "Switching Protocols"},
     {200, "OK"},
     {201, "Created"},
     {202, "Accepted"},
+    {203, "Non-Authoritative Information"},
     {204, "No Content"},
     {205, "Reset Content"},
     {206, "Partial Content"},
+    {207, "Multi-Status"},
+    {300, "Multiple Choices"},
     {301, "Moved"},
     {302, "Found"},
     {303, "See Other"},
     {304, "Not Modified"},
     {305, "Use Proxy"},
+    {307, "Temporary Redirect"},
     {400, "Bad Request"},
     {401, "Unauthorized"},
+    {402, "Payment Required"},
     {403, "Forbidden"},
     {404, "Not Found"},
     {405, "Method Not Allowed"},
@@ -84,12 +91,16 @@ static struct {
     {413, "Request Entity Too Large"},
     {414, "Request-URI Too Long"},
     {415, "Unsupported Media Type"},
+    {416, "Requested range not satisfiable"},
+    {417, "Expectation Failed"},
+    {423, "Locked"},
     {500, "Internal Server Error"},
     {501, "Not Implemented"},
     {502, "Bad Gateway"},
     {503, "Service Unavailable"},
     {504, "Gateway Timeout"},
-    {505, "HTTP Version Not Supported"}
+    {505, "HTTP Version Not Supported"},
+    {507, "Insufficient Storage"}
 };
 
 /*
