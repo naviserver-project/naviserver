33d23bb0209189088b6aa0b48a2faafd8335f425
Vlad Seryakov <vlad@crystalballinc.com>
2005-03-24 01:17:54 +0000

    Add missing pubic Ns_SockBindUdp declaration

--- include/ns.h
+++ include/ns.h
@@ -1084,6 +1084,7 @@ NS_EXTERN SOCKET Ns_SockListenEx(char *address, int port, int backlog);
 NS_EXTERN SOCKET Ns_SockListenUdp(char *address, int port);
 NS_EXTERN SOCKET Ns_SockListenRaw(int proto);
 NS_EXTERN SOCKET Ns_SockListenUnix(char *path);
+NS_EXTERN SOCKET Ns_SockBindUdp(struct sockaddr_in *saPtr);
 
 /*
  * sock.c:
