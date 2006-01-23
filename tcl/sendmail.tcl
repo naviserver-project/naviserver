1dd33491c3df715885152e3916e8d034410cbe73
Bernd Eidenschink <eidenschink@web.de>
2006-01-23 10:09:09 +0000

    (ns_sendmail): add character encoding of subject, body; quoted-printable; AUTH-PLAIN
    * tcl/sendmail.tcl: Added character encoding for subject and body,
    message-id creation, quoted printable text and AUTH PLAIN
    mechanism (all optional).
    
    * doc/src/ns_sendmail.man:
    * doc/src/README.doc:
    * doc/man/ns_sendmail.n:
    * doc/html/ns_sendmail.html: Doctools documentation file for new
    ns_sendmail functionality as well as examples created with dtplite

--- tcl/sendmail.tcl
+++ tcl/sendmail.tcl
@@ -27,22 +27,33 @@
 # version of this file under either the License or the GPL.
 #
 
+# $Header$
+
 #
 # sendmail.tcl - Define the ns_sendmail procedure for sending
 # email from a Tcl script through a remote SMTP server.
 #
 
-proc _ns_smtp_send { mode wfp string timeout} {
- 
-   if {[lindex [ns_sockselect -timeout $timeout {} $wfp {}] 1] == ""} {
-     error "$mode: Timeout writing to SMTP host"
+proc _ns_smtp_send { mode wfp string timeout } {
+
+    if {[ns_config -bool ns/parameters "smtplogmode" false]} {
+        ns_log notice "S: $mode $wfp $string $timeout"
+        return ""
+    }
+
+    if {[lindex [ns_sockselect -timeout $timeout {} $wfp {}] 1] == ""} {
+        error "$mode: Timeout writing to SMTP host"
     }
     puts $wfp $string\r
     flush $wfp
 }
 
 proc _ns_smtp_recv { mode rfp check timeout { error 1 } } {
-    
+
+    if {[ns_config -bool ns/parameters "smtplogmode" false]} {
+        return ""
+    }
+
     while (1) {
       if {[lindex [ns_sockselect -timeout $timeout $rfp {} {}] 0] == ""} {
         error "$mode: Timeout reading from SMTP host"
@@ -57,6 +68,61 @@ proc _ns_smtp_recv { mode rfp check timeout { error 1 } } {
       }
       if ![string match "-" [string range $line 3 3]] { break }
     }
+}
+
+proc ns_sendmail_config { { mode "" } } {
+    set myset [ns_set create smtpconfiguration \
+    smtphost          [ns_config ns/parameters smtphost] \
+    smtpport          [ns_config ns/parameters smtpport] \
+    smtptimeout       [ns_config ns/parameters smtptimeout] \
+    smtplogmode       [ns_config ns/parameters smtplogmode] \
+    smtpmsgid         [ns_config ns/parameters smtpmsgid] \
+    smtpmsgidhostname [ns_config ns/parameters smtpmsgidhostname] \
+    smtpencodingmode  [ns_config ns/parameters smtpencodingmode] \
+    smtpencoding      [ns_config ns/parameters smtpencoding] \
+    smtpauthuser      [ns_config ns/parameters smtpauthuser] \
+    smtpauthpassword  [ns_config ns/parameters smtpauthpassword]]
+    if { $mode eq "log" } {
+        ns_log notice [ns_set print $myset]
+        return
+    }
+    return [ns_set array $myset]
+}
+
+proc _ns_sendmail_qp { str } {
+
+   # first step: quote characters where necessary 
+   regsub -all -- \
+       {[\x00-\x08\x0B-\x1E\x21-\x24\x3D\x40\x5B-\x5E\x60\x7B-\xFF]} \
+       $str {[format =%02X [scan "\\&" %c]]} str
+   set str [subst -novariable $str]
+
+   # second step: handle some special cases
+   set _map [list "\t\n" "=09\n" " \n" "=20\n" "\n\.\n" "\n=2E\n"]
+
+   return [string map $_map $str]
+
+}
+
+proc _ns_sendmail_breaklines { str } {
+
+    set brokenlines ""
+    foreach line [split $str "\n"] {
+        while {[string length $line] > 72} {
+            if {[regexp -indices -- {(=|=.)$} [string range $line 0 71] _findex]} {
+                set _index_fragment [expr {[lindex $_findex 0] -1}]
+                append brokenlines "[string range $line 0 $_index_fragment]=\n"
+                incr _index_fragment +1
+                set line [string range $line $_index_fragment end]
+            } else {
+                regexp -- {(.{72})(.*)} $line all first_72 line
+                append brokenlines "${first_72}=\n"
+            }
+        }
+        append brokenlines "${line}\n"
+    }
+    return [regsub -- {\n$} $brokenlines ""]
+
 }
 
 proc ns_sendmail { to from subject body {headers {}} {bcc {}} {cc {}} } {
@@ -76,53 +142,77 @@ proc ns_sendmail { to from subject body {headers {}} {bcc {}} {cc {}} } {
         append bcc , $addr
       }
     }
-    
+
     ## Get smtp server into, if none then use localhost
     set smtp [ns_config ns/parameters smtphost]
-    if [string match "" $smtp] { set smtp [ns_config ns/parameters mailhost] }
-    if [string match "" $smtp] { set smtp localhost }
-    set timeout [ns_config ns/parameters smtptimeout]
-    if [string match "" $timeout] { set timeout 60 }
-    set smtpport [ns_config ns/parameters smtpport]
-    if [string match "" $smtpport] { set smtpport 25 }
+    if [string match "" $smtp] { 
+        set smtp [ns_config ns/parameters mailhost "localhost"] 
+    }
+    set timeout [ns_config ns/parameters smtptimeout "60"]
+    set smtpport [ns_config ns/parameters smtpport "25"]
+    ## Apply an encoding if configured; by default off
+    if {[ns_config ns/parameters smtpencodingmode false]} {
+        set target_encoding [ns_config ns/parameters smtpencoding "utf-8"]
+        if {[string length $subject] != [string bytelength [encoding convertto $target_encoding $subject]]} {
+            # just to satisfy pedantic spam rules (0.0 points anyway):
+            # 0.0 SUBJECT_EXCESS_QP Subject: quoted-printable encoded unnecessarily
+            set subject "=?$target_encoding?Q?[_ns_sendmail_qp [encoding convertto $target_encoding $subject]]?="
+        }
+        set body [_ns_sendmail_breaklines [_ns_sendmail_qp [encoding convertto $target_encoding $body]]]
+        if { $headers == "" } {
+            set headers [ns_set create headers]
+            ns_set put $headers "MIME-Version" "1.0"
+            ns_set put $headers "Content-Type" "text/plain; charset=\"${target_encoding}\"" 
+            ns_set put $headers "Content-Transfer-Encoding" "quoted-printable" 
+        } else {
+            if {[ns_set iget $headers "content-type"] eq "" } {
+                ns_set put $headers "Content-Type" "text/plain; charset=\"${target_encoding}\"" 
+            }
+            if {[ns_set iget $headers "content-transfer-encoding"] eq "" } {
+                ns_set put $headers "Content-Transfer-Encoding" "quoted-printable" 
+            }
+            if {[ns_set iget $headers "mime-version"] eq ""} {
+                ns_set put $headers "MIME-Version" "1.0"
+            }
+        }
+    }
 
-    ## Extract "from" email address
-    if [regexp {.*<(.*)>} $from d address] { set from $address }
-    
     ## Prepare to,cc,bcc address lists
     set tolist [list]
-    regsub -all {[\n\r ]} $to {} to
+    regsub -all {[\n\r]} $to {} to
     foreach addr [split $to ,] {
-      if { [regexp {.*<(.*)>} $addr d address] } { set addr $address }
       if { [set addr [string trim $addr]] != "" } { lappend tolist $addr }
     }
 
     set cclist [list]
-    regsub -all {[\n\r ]} $cc {} cc
+    regsub -all {[\n\r]} $cc {} cc
     foreach addr [split $cc ,] {
-      if { [regexp {.*<(.*)>} $addr d address] } { set addr $address }
       if { [set addr [string trim $addr]] != "" } { lappend cclist $addr }
     }
     
     set bcclist [list]
-    regsub -all {[\n\r ]} $bcc {} bcc
+    regsub -all {[\n\r]} $bcc {} bcc
     foreach addr [split $bcc ,] {
-      if { [regexp {.*<(.*)>} $addr d address] } { set addr $address }
       if { [set addr [string trim $addr]] != "" } { lappend bcclist $addr }
     }
-    
+
     ## Send it along to _ns_sendmail
     _ns_sendmail $smtp $smtpport $timeout $tolist $cclist $bcclist \
 	    $from $subject $body $headers
 }
 
 proc _ns_sendmail {smtp smtpport timeout tolist cclist bcclist from subject body headers} {
-    
+
     ## Put the tolist in the headers
     set rfcto [join $tolist ", "]
-    
+
     ## Build headers
-    set msg "To: $rfcto\nFrom: $from\nSubject: $subject\nDate: [ns_httptime [ns_time]]\n"
+    set _date [ns_httptime [ns_time]]
+
+    set msg "To: $rfcto\nFrom: $from\nSubject: $subject\nDate: $_date\n"
+
+    ## make sure we only work with the address itself from here on
+    regexp {.*<(.*)>} $from null from
 
     ## CC recipients in separate header
     if { $cclist != "" } { append msg "Cc: [join $cclist ","]\n" }
@@ -134,7 +224,19 @@ proc _ns_sendmail {smtp smtpport timeout tolist cclist bcclist from subject body
 	append msg "[ns_set key $headers $i]: [ns_set value $headers $i]\n"
       }
     }
-    
+    ## If no Message-ID is specified, produce one if smtpmsgid is true
+    ## See RFC2822(message identifier)
+    if { [ns_config ns/parameters smtpmsgid false] && ($headers eq "" || \
+             ($headers ne "" && [ns_set iget $headers "message-id"] eq ""))} {
+        if {[set _msg_0 [ns_config ns/parameters smtpmsgidhostname]] eq ""} {
+            set _msg_0 [ns_info hostname]
+        }
+	set _msg_1 [string range [ns_sha1 "$from$tolist$subject[clock seconds][ns_info threads]"] 0 14]
+        set _msg_2 [clock format [clock seconds] -format "%Y%m%d"]
+	set _msg_id "$_msg_1.$_msg_2@$_msg_0"
+	append msg "Message-ID: <$_msg_id>\n"
+    }
+
     ## Blank line between headers and body
     append msg "\n$body\n"
     
@@ -145,23 +247,47 @@ proc _ns_sendmail {smtp smtpport timeout tolist cclist bcclist from subject body
       append data "\r\n"
     }
     append data .
-    
+
     ## Open the connection
     set sock [ns_sockopen $smtp $smtpport]
     set rfp [lindex $sock 0]
     set wfp [lindex $sock 1]
 
-    ## Perform the SMTP conversation
+    ## Perform the (E)SMTP conversation
     if { [catch {
       _ns_smtp_recv "Start" $rfp 220 $timeout
-      _ns_smtp_send "Helo" $wfp "HELO [ns_info hostname]" $timeout
-      _ns_smtp_recv "Helo" $rfp 250 $timeout
+
+      set _authuser [ns_config ns/parameters smtpauthuser]
+      set _authpass [ns_config ns/parameters smtpauthpassword]
+      set _hostname [ns_info hostname]
+      if {[ns_config ns/parameters smtpmsgidhostname] ne ""} {
+          set _hostname [ns_config ns/parameters smtpmsgidhostname]
+      }
+      if {$_authuser ne "" && $_authpass ne ""} {
+          _ns_smtp_send "EHLO" $wfp "EHLO $_hostname" $timeout
+          _ns_smtp_recv "EHLO" $rfp 250 $timeout
+
+          if {[llength [split $_authuser "\0"]]==1} {
+              # default case: equal user and realm
+              set _authtoken "${_authuser}\0${_authuser}\0${_authpass}"
+          } else {
+              # self constructed user and realm
+              set _authtoken "${_authuser}\0${_authpass}"
+          }
+          _ns_smtp_send "AUTH PLAIN" $wfp \
+              "AUTH PLAIN [ns_base64encode $_authtoken]" $timeout
+          _ns_smtp_recv "AUTH PLAIN" $rfp 235 $timeout
+      } else {
+          _ns_smtp_send "Helo" $wfp "HELO $_hostname" $timeout
+          _ns_smtp_recv "Helo" $rfp 250 $timeout
+      }
+
       _ns_smtp_send "Mail $from" $wfp "MAIL FROM:<$from>" $timeout
       _ns_smtp_recv "Mail $from" $rfp 250 $timeout
-	
       ## Loop through To list via multiple RCPT TO lines
       foreach to $tolist {
         if { $to == "" } { continue }
+        regexp {.*<(.*)>} $to null to
         _ns_smtp_send "Rcpt $to" $wfp "RCPT TO:<$to>" $timeout
         _ns_smtp_recv "Rcpt $to" $rfp 250 $timeout 0
       }
@@ -169,18 +295,20 @@ proc _ns_sendmail {smtp smtpport timeout tolist cclist bcclist from subject body
       ## Loop through CC list via multiple RCPT TO lines
       foreach to $cclist {
         if { $to == "" } { continue }
+        regexp {.*<(.*)>} $to null to
         _ns_smtp_send "Rcpt $to" $wfp "RCPT TO:<$to>" $timeout
 	_ns_smtp_recv "Rcpt $to" $rfp 250 $timeout 0
       }
-	
+
       ## Loop through BCC list via multiple RCPT TO lines
       ## A BCC should never, ever appear in the header.  Ever.  Not even.
       foreach to $bcclist {
         if { $to == "" } { continue }
+        regexp {.*<(.*)>} $to null to
         _ns_smtp_send "Rcpt $to" $wfp "RCPT TO:<$to>" $timeout
         _ns_smtp_recv "Rcpt $to" $rfp 250 $timeout 0
       }
-	
+
       _ns_smtp_send Data $wfp DATA $timeout
       _ns_smtp_recv Data $rfp 354 $timeout
       _ns_smtp_send Data $wfp $data $timeout
