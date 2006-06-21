2a3888536be40be296f78737cebb2f92ec1c79e3
Vlad Seryakov <vlad@crystalballinc.com>
2006-06-21 20:50:29 +0100

    Add online help for each parameter

--- contrib/nsconfig.tcl
+++ contrib/nsconfig.tcl
@@ -42,12 +42,170 @@
 #   Then restart the server and point the browser to http://localhost/_nsconfig
 #
 
-set path "ns/server/nsconfig"
+set server [ns_info server]
 
-nsv_set _ns_config ns:enabled       [set enabled [ns_config -bool $path enabled 0]]
-nsv_set _ns_config ns:url           [set url [ns_config $path url "/_nsconfig"]]
-nsv_set _ns_config ns:user          [ns_config $path user "naviserver"]
-nsv_set _ns_config ns:password      [ns_config $path password "admin"]
+nsv_set _ns_config ns:enabled       [set enabled [ns_config -bool ns/server/nsconfig enabled 0]]
+nsv_set _ns_config ns:url           [set url [ns_config ns/server/nsconfig url "/_nsconfig"]]
+nsv_set _ns_config ns:user          [ns_config ns/server/nsconfig user "naviserver"]
+nsv_set _ns_config ns:password      [ns_config ns/server/nsconfig password "admin"]
+
+# Help entries
+
+nsv_set _ns_config help.ns/servers.* "Server name if config is shared between multiple servers"
+
+nsv_set _ns_config help.ns/mimetypes.* "Mime type to return for file extension"
+
+nsv_set _ns_config help.ns/threads.stacksize "Stack size for every new thread, should be big enough to avoid crashes"
+
+nsv_set _ns_config help.ns/server/$server/modules.* "Module to load on server startup"
+
+nsv_set _ns_config help.ns/server/$server/tcl.errorlogheaders "List of connection headers to log for Tcl errors"
+nsv_set _ns_config help.ns/server/$server/tcl.initfile "Tcl file to perform initialization, init.tcl by default"
+nsv_set _ns_config help.ns/server/$server/tcl.library "Where all private Tcl modules are located"
+nsv_set _ns_config help.ns/server/$server/tcl.lazyloader "Set to true to use Tcl-trace based interp initialization"
+nsv_set _ns_config help.ns/server/$server/tcl.nsvbuckets "Number of buckets in Tcl hash table for nsv vars"
+
+nsv_set _ns_config help.ns/server/$server/redirects.* "HTTP return code redirect url"
+
+nsv_set _ns_config help.ns/server/$server/module/nssock.port "TCP port server will listen on"
+nsv_set _ns_config help.ns/server/$server/module/nssock.address "IP address for listener to bind on"
+nsv_set _ns_config help.ns/server/$server/module/nssock.hostname "Hostname to use in redirects"
+nsv_set _ns_config help.ns/server/$server/module/nssock.maxinput "Max upload size"
+nsv_set _ns_config help.ns/server/$server/module/nssock.maxline "Max line size in request or HTTP header"
+nsv_set _ns_config help.ns/server/$server/module/nssock.bufsize "Read-ahead buffer size"
+nsv_set _ns_config help.ns/server/$server/module/nssock.readahead "Max upload size when to use spooler"
+nsv_set _ns_config help.ns/server/$server/module/nssock.uploadsize "Max upload size when to use statistics"
+nsv_set _ns_config help.ns/server/$server/module/nssock.spoolerthreads "Number of spooler threads"
+nsv_set _ns_config help.ns/server/$server/module/nssock.writerthreads "Number of writer threads"
+nsv_set _ns_config help.ns/server/$server/module/nssock.writersize "Min return file size when to use writer"
+nsv_set _ns_config help.ns/server/$server/module/nssock.writerbufsize "Size of the send buffer for writer threads"
+nsv_set _ns_config help.ns/server/$server/module/nssock.readtimeoutlogging "Log timed-out waiting for complete request"
+nsv_set _ns_config help.ns/server/$server/module/nssock.serverrejectlogging "Log unable to match request to a virtual server"
+nsv_set _ns_config help.ns/server/$server/module/nssock.sockerrorlogging "Log malformed request, or would exceed request limits"
+nsv_set _ns_config help.ns/server/$server/module/nssock.sockshuterrorlogging "Log error while attempting to shutdown a socket during connection close"
+nsv_set _ns_config help.ns/server/$server/module/nssock.maxheaders "Max size of HTTP headers"
+nsv_set _ns_config help.ns/server/$server/module/nssock.sndbuf "Size of TCP send buffer"
+nsv_set _ns_config help.ns/server/$server/module/nssock.maxqueuesize "Max size of sockets in the queue"
+nsv_set _ns_config help.ns/server/$server/module/nssock.rcvbuf "Size of TCP receive buffer"
+nsv_set _ns_config help.ns/server/$server/module/nssock.sendwait "Timeout for sending data"
+nsv_set _ns_config help.ns/server/$server/module/nssock.recvwait "Timeout for receiving data"
+nsv_set _ns_config help.ns/server/$server/module/nssock.closewait "Timeout for closing socket"
+nsv_set _ns_config help.ns/server/$server/module/nssock.keepwait "Timeout for keep-alive requests"
+nsv_set _ns_config help.ns/server/$server/module/nssock.backlog "Number of sockets in the TCP listen queue"
+nsv_set _ns_config help.ns/server/$server/module/nssock.keepallmethods "Keepalive all methods or just GET"
+nsv_set _ns_config help.ns/server/$server/module/nssock.location "Full driver location"
+
+nsv_set _ns_config help.ns/server/$server/module/nslog.file "Path to the access log file"
+nsv_set _ns_config help.ns/server/$server/module/nslog.formattedtime "If true then use common log format"
+nsv_set _ns_config help.ns/server/$server/module/nslog.logcombined "If true then use NCSA combined format"
+nsv_set _ns_config help.ns/server/$server/module/nslog.logreqtime "Put in the log request elapsed time"
+nsv_set _ns_config help.ns/server/$server/module/nslog.maxbuffer "Max # of lines in the buffer, 0 ni limit"
+nsv_set _ns_config help.ns/server/$server/module/nslog.maxbackup "Max # of files to keep when rolling"
+nsv_set _ns_config help.ns/server/$server/module/nslog.rollhour "Time to roll log"
+nsv_set _ns_config help.ns/server/$server/module/nslog.rolllog "If true then do the log rolling"
+nsv_set _ns_config help.ns/server/$server/module/nslog.rollonsignal "If true then roll the log on SIGHUP"
+nsv_set _ns_config help.ns/server/$server/module/nslog.suppressquery "If true then don't show query string in the log"
+nsv_set _ns_config help.ns/server/$server/module/nslog.checkforproxy "If true ten check for X-Forwarded-For header"
+nsv_set _ns_config help.ns/server/$server/module/nslog.extendedheaders "List of additional headers to put in the log"
+
+nsv_set _ns_config help.ns/server/$server/module/nscp.address "IP Address to listen on"
+nsv_set _ns_config help.ns/server/$server/module/nscp.port "TCP port for telnet access"
+nsv_set _ns_config help.ns/server/$server/module/nscp.echopassword "Enable echoing password"
+nsv_set _ns_config help.ns/server/$server/module/nscp.cpcmdlogging "Enable command logging"
+
+nsv_set _ns_config help.ns/server/$server/module/vhost.enabled "enable/disable virtual hosting"
+nsv_set _ns_config help.ns/server/$server/module/vhost.hostprefix "Prefix between serverdir and host name"
+nsv_set _ns_config help.ns/server/$server/module/vhost.stripport "Remove :port in the Host: header when building pageroot path so Host: www.host.com:80 will result in pageroot serverdir/www.host.com"
+nsv_set _ns_config help.ns/server/$server/module/vhost.stripwww "Remove www. prefix from Host: header when building pageroot path so Host: www.host.com will result in pageroot serverdir/host.com"
+nsv_set _ns_config help.ns/server/$server/module/vhost.hosthashlevel "Hash the leading characters of string into a path, skipping periods and slashes. For example, given the string 'foo' and the levels 2, 3: foo, 2 -> /f/o"
+
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.exec "Proxy program to start"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.evaltimeout "# Timeout (ms) when evaluating scripts"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.gettimeout "# Timeout (ms) when getting proxy handles"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.sendtimeout "# Timeout (ms) to send data"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.recvtimeout "# Timeout (ms) to receive results"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.waittimeout "# Timeout (ms) to wait for slaveis to die"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.maxslaves "# Max number of allowed proxies slaves alive"
+nsv_set _ns_config help.ns/server/$server/module/nsproxy.minslaves "# Min number of proxy slaves alive"
+
+nsv_set _ns_config help.ns/server/$server.enabletclpages "Parse *.tcl files in pageroot if enabled"
+nsv_set _ns_config help.ns/server/$server.connsperthread "Normally there's one conn per thread"
+nsv_set _ns_config help.ns/server/$server.flushcontent "Flush all data before returning"
+nsv_set _ns_config help.ns/server/$server.maxconnections "Max connections to put on queue"
+nsv_set _ns_config help.ns/server/$server.maxthreads "Tune this to scale your server of max number of running threads"
+nsv_set _ns_config help.ns/server/$server.minthreads "Min number of idle threads"
+nsv_set _ns_config help.ns/server/$server.threadtimeout "Idle threads die at this rate"
+nsv_set _ns_config help.ns/server/$server.checkmodifiedsince "Honor If-Modifified-Since requests"
+nsv_set _ns_config help.ns/server/$server.preserve "Preserve header case"
+nsv_set _ns_config help.ns/server/$server.urlstats "Enable/disable url statistics"
+nsv_set _ns_config help.ns/server/$server.realm "Ralm to use in Basic Authentiction"
+nsv_set _ns_config help.ns/server/$server.noticedetail "Append server/host info in notice responses"
+nsv_set _ns_config help.ns/server/$server.errorminsize "Min size of error messages, covers MSIE bug"
+
+nsv_set _ns_config help.ns/server/stats.enabled "Enable/disable statistics web page"
+nsv_set _ns_config help.ns/server/stats.url "Url where to access statistics webpage"
+nsv_set _ns_config help.ns/server/stats.user "User name for access"
+nsv_set _ns_config help.ns/server/stats.password "Password for access"
+
+nsv_set _ns_config help.ns/server/nsconfig.enabled "Enable/disable config web page"
+nsv_set _ns_config help.ns/server/nsconfig.url "Url where to access config webpage"
+nsv_set _ns_config help.ns/server/nsconfig.user "User name for access, if empty no basic authentication will be used"
+nsv_set _ns_config help.ns/server/nsconfig.password "Password for access"
+
+nsv_set _ns_config help.ns/server/$server/adp.map "Extensions to parse as ADP's"
+nsv_set _ns_config help.ns/server/$server/adp.enableexpire "Set 'Expires: now' on all ADP's"
+nsv_set _ns_config help.ns/server/$server/adp.enabledebug "Allow Tclpro debugging with ?debug."
+nsv_set _ns_config help.ns/server/$server/adp.startpage "ADP start page to use for empty ADP requests"
+nsv_set _ns_config help.ns/server/$server/adp.errorpage "ADP error page"
+nsv_set _ns_config help.ns/server/$server/adp.defaultparser "Default parser to use for parsing ADP pages"
+nsv_set _ns_config help.ns/server/$server/adp.cachesize "Cache size for ADP compiled pages"
+
+nsv_set _ns_config help.ns/server/$server/adp/compress.enable "Enable/disable on-the-fly compression"
+nsv_set _ns_config help.ns/server/$server/adp/compress.level "Compression level"
+nsv_set _ns_config help.ns/server/$server/adp/compress.minsize "Min size fo the file for compression"
+
+nsv_set _ns_config help.ns/server/$server/fastpath.serverdir "Defines absolute path to server's home directory"
+nsv_set _ns_config help.ns/server/$server/fastpath.pagedir "Defines absolute or relative to serverdir directory where all html/adp pages are located"
+nsv_set _ns_config help.ns/server/$server/fastpath.cache "Enable cache for normal URLs. Optional, default is false"
+nsv_set _ns_config help.ns/server/$server/fastpath.cachemaxsize "Size of fast path cache. Optional, default is 5120000"
+nsv_set _ns_config help.ns/server/$server/fastpath.cachemaxentry "Largest file size allowed in cache. Optional, default is cachemaxsize / 10"
+nsv_set _ns_config help.ns/server/$server/fastpath.mmap "Use mmap() for cache. Optional, default is false"
+nsv_set _ns_config help.ns/server/$server/fastpath.directoryfile "List of directory index/default page to look for"
+nsv_set _ns_config help.ns/server/$server/fastpath.directorylisting "Directory listing style. Optional, Can be fancy or simple"
+nsv_set _ns_config help.ns/server/$server/fastpath.directoryproc "Name of Tcl proc to use to display directory listings. Optional, default is to use _ns_dirlist. You can either specify directoryproc, or directoryadp - not both"
+nsv_set _ns_config help.ns/server/$server/fastpath.directoryadp "Name of ADP page to use to display directory listings. Optional. You can either specify directoryadp or directoryproc - not both"
+
+nsv_set _ns_config help.ns/parameters.dbcloseonexit "Close DB handles on server exit if driver needs graceful shutdown"
+nsv_set _ns_config help.ns/parameters.tclinitlock "Enable/disable Tcl locking on init"
+nsv_set _ns_config help.ns/parameters.tcllibrary "Where all shared Tcl modules are located"
+nsv_set _ns_config help.ns/parameters.home "Home directory where server is installed"
+nsv_set _ns_config help.ns/parameters.debug "Enable debugging log enteies"
+nsv_set _ns_config help.ns/parameters.serverlog "Path to the main server log file"
+nsv_set _ns_config help.ns/parameters.pidfile "Path to the server pid file"
+nsv_set _ns_config help.ns/parameters.hackcontenttype "Automatic adjustment of response content-type header to include charset"
+nsv_set _ns_config help.ns/parameters.outputcharset "Default output charset.  When none specified, no character encoding of output is performed"
+nsv_set _ns_config help.ns/parameters.urlcharset "Default Charset for Url Encode/Decode. When none specified, no character set encoding is performed"
+nsv_set _ns_config help.ns/parameters.preferredcharsets "This parameter supports output encoding arbitration"
+nsv_set _ns_config help.ns/parameters.logroll "Enable/disable log file rolling"
+nsv_set _ns_config help.ns/parameters.logdebug "Log debugginf messages"
+nsv_set _ns_config help.ns/parameters.logdev "Log development messages"
+nsv_set _ns_config help.ns/parameters.lognotice "Log notice messages"
+nsv_set _ns_config help.ns/parameters.logusec "Logging usec as well"
+nsv_set _ns_config help.ns/parameters.logexpanded "Expanded log with extra newlines"
+nsv_set _ns_config help.ns/parameters.logmaxbackup "Max number of backup files"
+nsv_set _ns_config help.ns/parameters.logmaxlevel "Max severity level for logging"
+nsv_set _ns_config help.ns/parameters.logmaxbuffer ""
+nsv_set _ns_config help.ns/parameters.smtphost "Host of the SMTP server, localhost is default"
+nsv_set _ns_config help.ns/parameters.smtptimeout "Timeout for SMTP connections"
+nsv_set _ns_config help.ns/parameters.user "User name the server is running as"
+nsv_set _ns_config help.ns/parameters.group "Group name the server is running as"
+nsv_set _ns_config help.ns/parameters.shutdowntimeout "Timeout for waiting threads to shutdown"
+nsv_set _ns_config help.ns/parameters.schedmaxelapsed "Max time for scheduled procs to run in non-threaded mode"
+nsv_set _ns_config help.ns/parameters.listenbacklog "Number of sockets to use in listen call"
+nsv_set _ns_config help.ns/parameters.dnscache "Enable/disable dns caching"
+nsv_set _ns_config help.ns/parameters.dnscachemaxsize "Max size of dns cache"
+nsv_set _ns_config help.ns/parameters.dnswaittimeout "Timeout for dns resolver"
+nsv_set _ns_config help.ns/parameters.dnscachetimeout "Timeout for dns cache updates"
 
 if { $enabled }  {
     ns_register_proc GET $url/* _ns_config.handleUrl
@@ -196,12 +354,19 @@ proc _ns_config.section {} {
      }
     }
 
+    # All possible parameters for this section
+    foreach param [_ns_config.sectionParams $section] {
+      set params($param) 1
+    }
     set html [_ns_config.header $title]
 
     append html "<table border=0 cellpadding=5 cellspacing=5>"
     foreach key [lsort [nsv_array names _ns_config $section|*]] {
       set value [nsv_get _ns_config $key]
-      set key [lindex [split $key |] 1]
+      set key [string tolower [lindex [split $key |] 1]]
+      if { [info exists params($key)] } {
+          unset params($key)
+      }
       append html "<tr><td class=lbl>$key:</td>
                        <td><input type=text size=40 name=\"$key\" value=\"$value\">
                            <a href=section.adp?@cmd=r&@section=$section&@key=$key
@@ -209,10 +374,20 @@ proc _ns_config.section {} {
                               title=\"Remove entry\"
                               onClick=\"return confirm('Remove $key?')\">(X)</a>
                        </td>
+                       <td style=\"font-size:7pt;color:gray;\"><i>[_ns_config.sectionHelp $section $key]</i></td>
                    </tr>"
     }
-    append html "<tr><td colspan=2 style=\"color: #666699; border-top: 1px solid #000000;\">New parameter:</td></tr>
-                 <tr><td><input type=text size=10 name=@name></td>
+    set selectbox ""
+    foreach param [lsort [array names params]] {
+      append selectbox "<option>$param"
+    }
+    if { $selectbox != "" } {
+        set selectbox "<select style=\"font-size:7pt;\" onChange=\"this.form.elements\['@name'\].value=this.value\">
+                       <option value=''>--$selectbox
+                       </select><br>"
+    }
+    append html "<tr><td colspan=2 style=\"color: #666699; border-top: 1px solid #000000;\">New parameter: $selectbox</td></tr>
+                 <tr><td><input type=text size=20 name=@name></td>
                      <td><input type=text size=40 name=@value></td>
                  </tr>
                  </table><hr>
@@ -381,7 +556,25 @@ proc _ns_config.sectionInit { name section } {
     }
 }
 
-proc _ns_config.fmtSeconds {seconds} {
+proc _ns_config.sectionHelp { section name } {
+
+    set help ""
+    foreach key [nsv_array names _ns_config help.$section.*] {
+      if { [string match -nocase $key help.$section.$name] } { return [nsv_get _ns_config $key] }
+    }
+    return $help
+}
+
+proc _ns_config.sectionParams { section } {
+
+    set params ""
+    foreach key [nsv_array names _ns_config help.$section.*] {
+      if { [string index $key end] != "*" } { lappend params [lindex [split $key .] end] }
+    }
+    return $params
+}
+
+proc _ns_config.fmtSeconds { seconds } {
 
     if { $seconds < 60 } {
         return "${seconds} (s)"
