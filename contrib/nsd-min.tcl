965e45380bdda14df265d7a53318a3479616ba39
Vlad Seryakov <vlad@crystalballinc.com>
2006-09-07 18:40:44 +0100

    Added comments to minimal nsd.tcl config

--- contrib/nsd-min.tcl
+++ contrib/nsd-min.tcl
@@ -1,6 +1,10 @@
+#
+# This is minimal Naviserver config file that makes the server to
+# accept HTTP requests on port 80 and accepts telnet into nscp on 127.0.0.1:2080
+# Logs are in the logs/nsd.log and logs/access.log
+#
 
 ns_section      "ns/server/default/modules"
-ns_param        nsdb            nsdb.so
 ns_param        nscp            nscp.so
 ns_param        nssock          nssock.so
 ns_param        nslog           nslog.so 
