7d78898ea92193d400b23322efea8797d037efbf
Stephen Deasey <sdeasey@gmail.com>
2007-05-24 18:24:04 +0100

    Add basic test of Tcl page processing, short and long pages

---
+++ tests/testserver/pages/helloworld.tcl
@@ -0,0 +1,10 @@
+#
+# A simple Tcl page.
+#
+# $Header$
+#
+
+set data   [ns_queryget data "Hello World!"]
+set repeat [ns_queryget repeat 1]
+
+ns_return 200 text/plain [string repeat $data $repeat]
