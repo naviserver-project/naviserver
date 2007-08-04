bffd109a3d597d149a8ece78381eb09f39dbffb5
Stephen Deasey <sdeasey@gmail.com>
2007-08-04 22:58:21 +0100

    Really fix the nsproxy tests. For sure

---
+++ nsproxy/test-nsproxy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# test-nsproxy.sh --
+#
+#   Make sure the nsproxy executable can find the newly built libraries.
+#   This wrapper script is specified to be called from: tests/test.nscfg
+#
+# $Header$
+#
+
+LD_LIBRARY_PATH="../nsd:../nsthread:../nsproxy:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH
+
+exec ../nsproxy/nsproxy $@
