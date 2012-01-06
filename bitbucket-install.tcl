6ceeddfd3649e1845ee430472f67fbfc25cbc033
Gustaf Neumann <neumann@wu-wien.ac.at>
2012-01-06 16:27:21 +0100

    Initial checkin of simple installer from bitbucket (requires wget)

---
+++ bitbucket-install.tcl
@@ -0,0 +1,30 @@
+set pageName [ns_queryget file ""]
+switch -exact $pageName {
+  nsconf.tcl -
+  nsstats.tcl {
+    set source https://bitbucket.org/naviserver/[file root $pageName]/get/tip.tar.gz
+    set page /opt/local/ns/pages/$pageName
+    if {![file readable $page]} {
+      exec wget --quiet --no-check-certificate -O /tmp/nsstats.tar.gz $source
+      if {[catch {exec tar Ozxvf /tmp/nsstats.tar.gz *$pageName > $page} errorMsg]} {
+	ns_log notice "error: $errorMsg"
+	if {[lindex $errorMsg 0] ne "x"} {
+	  ns_return 200 text/html \
+	      "<html><body>error while downloading $pageName: <b>$errorMsg</b>. <a href='/'>return</a>" 
+	}
+      }
+      ns_return 200 text/html \
+	  "<html><body>$pageName successfully downloaded and installed. <a href='/'>return</a>" 
+    }
+    ns_return 200 text/html \
+	"<html><body>$pageName already installed. <a href='/'>return</a>" 
+  }
+  default {
+    set pageName [string map [list < "&lt;" > "&gt;"] $pageName]
+    ns_return 200 text/html "<html><body>page $pageName unknown. <a href='/'>return</a>" 
+  }
+}
+
+
+
+
