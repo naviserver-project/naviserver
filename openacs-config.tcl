d9f30a0b9d30772389aa9f7c746ee788f2a03b6d
Gustaf Neumann <neumann@wu-wien.ac.at>
2012-12-01 22:59:22 +0100

    add documentation for highwatermark and lowwatermark

--- openacs-config.tcl
+++ openacs-config.tcl
@@ -145,14 +145,14 @@ ns_section ns/server/${server}
 	#
 	# Scaling and Tuning Options
 	#
-	#ns_param   maxconnections	100	;# 100, number of allocated connection stuctures
-	#ns_param   maxthreads		10	;# 10, maximal number of connection threads
-	#ns_param   minthreads          1	;# 1, minimal number of connection threads
-	ns_param   connsperthread	100	;# 0, number of connections (requests) handled per thread
-	#ns_param   threadtimeout	120	;# 120, timeout for idle theads
-	#ns_param   spread		0	;# 20, spread factor in percent for varying connsperthread and threadtimeout
-        ns_param concurrentcreatethreshold 100  ;# 80; allow concrruent creates when queue is fully beyond this percentage
-                                                ;# 100 is a concervative value, disabling concurrent creates
+	#ns_param	maxconnections	100	;# 100, number of allocated connection stuctures
+	#ns_param	maxthreads	10	;# 10, maximal number of connection threads
+	#ns_param	minthreads	1	;# 1, minimal number of connection threads
+	ns_param	connsperthread	100	;# 0, number of connections (requests) handled per thread
+	#ns_param	threadtimeout	120	;# 120, timeout for idle theads
+        #ns_param	lowwatermark	10      ;# 10; create additional threads above this queue-full percentage
+        ns_param	highwatermark	100     ;# 80; allow concurrent creates above this queue-is percentage
+                                                ;# 100 means to disable concurrent creates
 	#
 	# Directory listing options
 	#
