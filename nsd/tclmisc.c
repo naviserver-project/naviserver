a0b486855aaa38ecb47f7ab7cf8bb7a53d8b8094
Gustaf Neumann <neumann@wu-wien.ac.at>
2014-09-25 08:40:15 +0200

    - reduce variable scope

--- nsd/tclmisc.c
+++ nsd/tclmisc.c
@@ -1171,9 +1171,11 @@ static void MD5Transform(uint32_t buf[4], uint32_t const in[16]);
  */
 static void byteReverse(unsigned char *buf, unsigned longs)
 {
-    uint32_t t;
     do {
-	t = (uint32_t) ((unsigned) buf[3] << 8 | buf[2]) << 16 |
+	uint32_t t;
+
+	t = (uint32_t) 
+	    ((unsigned) buf[3] << 8 | buf[2]) << 16 |
 	    ((unsigned) buf[1] << 8 | buf[0]);
 	*(uint32_t *) buf = t;
 	buf += 4;
