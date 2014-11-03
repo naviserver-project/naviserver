b79eeb1c63dcd8fd61ea5d26882addac71d07226
Gustaf Neumann <neumann@wu-wien.ac.at>
2014-11-03 12:34:37 +0100

    - don't calculate with signed characters

--- nsd/httptime.c
+++ nsd/httptime.c
@@ -369,7 +369,7 @@ MakeNum(const char *s)
     if (*s >= '0' && *s <= '9') {
         return (10 * (*s - '0')) + (*(s + 1) - '0');
     } else {
-        return *(s + 1) - '0';
+	return *(s + 1) - UCHAR('0');
     }
 }
 
