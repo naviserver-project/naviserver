15b438ad401187f1b5e2ae4f5c4b2907177d0414
Gustaf Neumann <neumann@wu-wien.ac.at>
2015-01-16 14:12:20 +0100

    - win32: use GetCurrentProcessId() instead of _getpid() to return DWORD instead of int

--- include/nsthread.h
+++ include/nsthread.h
@@ -151,7 +151,7 @@ typedef int32_t ssize_t;
 #  define chsize                      _chsize
 #  define close                       _close
 #  define fileno                      _fileno
-#  define getpid                      _getpid
+#  define getpid                      GetCurrentProcessId
 #  define mktemp                      _mktemp
 #  define open                        _open
 #  define putenv                      _putenv
