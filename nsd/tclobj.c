f0c15e5cf4aec57d9fccca2aa67281ca6e8944b7
Gustaf Neumann <neumann@wu-wien.ac.at>
2015-04-27 16:30:54 +0200

    - don't cast away const

--- nsd/tclobj.c
+++ nsd/tclobj.c
@@ -419,7 +419,7 @@ UpdateStringOfAddr(Tcl_Obj *objPtr)
     char        buf[128];
     int         len;
 
-    len = snprintf(buf, sizeof(buf), "t%p-a%p-%s", (void *)type, (void *)addr, type);
+    len = snprintf(buf, sizeof(buf), "t%p-a%p-%s", (const void *)type, (const void *)addr, type);
     Ns_TclSetStringRep(objPtr, buf, len);
 }
 
