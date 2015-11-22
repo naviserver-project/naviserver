b74b9b1c5d8bfeb8f3185488c90730acc0ca7ab4
Gustaf Neumann <neumann@wu-wien.ac.at>
2015-11-22 20:21:59 +0100

    - fix test on wrong variable

--- nsperm/nsperm.c
+++ nsperm/nsperm.c
@@ -1219,7 +1219,7 @@ static int AllowDenyObjCmd(ClientData data, Tcl_Interp * interp, int objc, Tcl_O
     unsigned int flags = 0U;
 
     Ns_ObjvSpec opts[] = {
-        {"-noinherit", Ns_ObjvBool,   &flags,  INT2PTR(NS_TRUE)},
+        {"-noinherit", Ns_ObjvBool,   &noinherit,  INT2PTR(NS_TRUE)},
         {"--",         Ns_ObjvBreak,  NULL,    NULL},
         {NULL, NULL, NULL, NULL}
     };
@@ -1232,7 +1232,9 @@ static int AllowDenyObjCmd(ClientData data, Tcl_Interp * interp, int objc, Tcl_O
     if (Ns_ParseObjv(opts, args, interp, 2, objc, objv) != NS_OK) {
         return TCL_ERROR;
     }
-    if (noinherit != 0) {flags |= NS_OP_NOINHERIT;}
+    if (noinherit != 0) {
+        flags |= NS_OP_NOINHERIT;
+    }
 
     /*
      * Construct the base url.
