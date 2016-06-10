fca46a7f1eaa252fb8286e68d8a1d550c5f4dc3a
Gustaf Neumann <neumann@wu-wien.ac.at>
2016-06-10 12:23:52 +0200

    - improve error message

--- nsssl/nsssl.c
+++ nsssl/nsssl.c
@@ -203,7 +203,7 @@ Ns_ModuleInit(const char *server, const char *module)
      */
     value = Ns_ConfigGetValue(path, "certificate");
     if (value == NULL) {
-        Ns_Log(Error, "nsssl: certificate parameter should be specified under %s", path);
+        Ns_Log(Error, "nsssl: certificate parameter must be specified in the config file under %s", path);
         return NS_ERROR;
     }
     if (SSL_CTX_use_certificate_chain_file(drvPtr->ctx, value) != 1) {
