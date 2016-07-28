2e89e1552d22eb0b158229ee18fda8227fa9616c
Gustaf Neumann <neumann@wu-wien.ac.at>
2016-07-28 12:21:43 +0200

    - reduce verbosity

--- tests/testserver/modules/listencallback-procs.tcl
+++ tests/testserver/modules/listencallback-procs.tcl
@@ -1,12 +1,12 @@
 
 namespace eval ::ns_listencallback {
     proc conn_handler {rfd wfd} {
-	ns_log notice "--- conn_handler called with $rfd $wfd"
+	#ns_log notice "--- conn_handler called with $rfd $wfd"
 	puts $wfd "Welcome to the test server"
 	flush $wfd
-	ns_log notice "--- conn_handler reads from $rfd"
+	#ns_log notice "--- conn_handler reads from $rfd"
 	gets $rfd line
-	ns_log notice "--- conn_handler got <$line>"
+	#ns_log notice "--- conn_handler got <$line>"
 	puts $wfd "Well isn't that nice"
 	flush $wfd
     }
