0b63d0ddc06fe97057b948f7bde7120d57aa498e
Gustaf Neumann <neumann@wu-wien.ac.at>
2017-01-05 16:31:02 +0100

    - improve minimal sample config file

--- simple-config.tcl
+++ simple-config.tcl
@@ -1,10 +1,11 @@
 #
 # This is minimal NaviServer config file that makes the server to
-# accept HTTP requests on port 80
+# accept HTTP requests on [::]:80 (IPv6) or on 0.0.0.0:80 (IPv4)
 #
 # Logs are in the logs/nsd.log and logs/access.log
 #
-# Once nscp module is enabled it will accept telnet into nscp on 127.0.0.1:2080
+# Once nscp module is enabled it will accept telnet into nscp on
+# [::1]:2080 (IPv6) or 127.0.0.1:2080 (IPv4)
 #
 
 ns_section      "ns/servers"
@@ -15,3 +16,5 @@ ns_param         nscp            nscp.so
 ns_param         nssock          nssock.so
 ns_param         nslog           nslog.so
 
+ns_section     "ns/server/default/adp"
+ns_param        map              /*.adp
