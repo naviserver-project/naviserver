07c5cf086c91af903a100397095b6330c0b9aa1c
Gustaf Neumann <neumann@wu-wien.ac.at>
2017-07-24 11:22:07 +0200

    update interface

--- nsd/dns.c
+++ nsd/dns.c
@@ -222,7 +222,7 @@ DnsGet(GetProc *getProc, Ns_DString *dsPtr, Ns_Cache *cache, const char *key, bo
                     Ns_CacheSetValueExpires(entry, ns_strdup(ds.string),
                                             (size_t)ds.length, &endTime,
                                             (int)(diffTime.sec * 1000000 + diffTime.usec),
-                                            0u);
+                                            0u, 0u);
                 }
                 Ns_CacheBroadcast(cache);
             } else {
