66e28085e37a8f1f0e7c212a9708ad051213d554
Zoran Vasiljevic <zv@archiware.com>
2017-11-08 15:56:40 +0100

    Ns_TclGetOpaqueFromObj is now clever enough to obtain the address of the object by just looking at the object's string rep.

--- nsd/tclobj.c
+++ nsd/tclobj.c
@@ -348,11 +348,17 @@ Ns_TclGetOpaqueFromObj(const Tcl_Obj *objPtr, const char *type, void **addrPtrPt
     NS_NONNULL_ASSERT(type != NULL);
     NS_NONNULL_ASSERT(addrPtrPtr != NULL);
 
-    if (objPtr->typePtr != &addrType
-        || objPtr->internalRep.twoPtrValue.ptr1 != (void *) type) {
-        result = TCL_ERROR;
-    } else {
+    if (objPtr->typePtr == &addrType
+        && objPtr->internalRep.twoPtrValue.ptr1 == (void *) type) {
         *addrPtrPtr = objPtr->internalRep.twoPtrValue.ptr2;
+    } else {
+	char s[32] = {0};
+	void *t = NULL;
+        if (sscanf(Tcl_GetString((Tcl_Obj *) objPtr), "t%20p-a%20p-%32s", &t, addrPtrPtr, s) != 3 
+            || strcmp(s, type) || t != (void *) type) {
+
+            result = TCL_ERROR;
+        }
     }
 
     return result;
