2dc5dbaa848f25c003a0a0e66cd6754709b45ebb
Gustaf Neumann <neumann@wu-wien.ac.at>
2019-01-29 12:01:54 +0100

    omit ::msgcat from blueprint

--- tcl/nstrace.tcl
+++ tcl/nstrace.tcl
@@ -645,7 +645,7 @@ ns_runonce {
             variable nsplist
             lappend nsplist $top
             foreach nsp [namespace children $top] {
-                if {$nsp eq "::tcl"} {continue}
+                if {$nsp in {"::tcl" "::msgcat"}} {continue}
                 _namespaces $nsp
             }
         }
