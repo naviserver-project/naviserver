8bcdee1a328d6e9a943ef2b8856b8e4aba347531
Gustaf Neumann <neumann@wu-wien.ac.at>
2020-12-05 16:32:02 +0100

    prefer config section with braces

--- simple-config.tcl
+++ simple-config.tcl
@@ -1,24 +1,25 @@
 #
-# This is minimal NaviServer configuration file that makes the server to
+# This is a fairly minimal NaviServer configuration file that makes the server to
 # accept HTTP requests on 0.0.0.0:8000 (IPv4)
 #
 # Logs are in the logs/nsd.log and logs/access.log
 #
-# Once nscp module is enabled it will accept telnet into nscp on
+# When the nscp module is enabled it will accept telnet into nscp on
 # [::1]:2080 (IPv6) or 127.0.0.1:2080 (IPv4)
 #
 
-ns_section "ns/servers"
-ns_param    default         NaviServer
-
-ns_section "ns/server/default/adp"
-ns_param    map              /*.adp
-
-ns_section "ns/server/default/modules"
-ns_param    nscp            nscp
-ns_param    nssock          nssock
-ns_param    nslog           nslog
-
-ns_section "ns/server/default/module/nssock"
-ns_param    address         0.0.0.0
-ns_param    port            8000
+ns_section ns/servers {
+    ns_param    default         NaviServer
+}
+ns_section ns/server/default/adp {
+    ns_param    map              /*.adp
+}
+ns_section ns/server/default/modules {
+    #ns_param   nscp            nscp
+    ns_param    nssock          nssock
+    ns_param    nslog           nslog
+}
+ns_section ns/server/default/module/nssock {
+    ns_param    address         0.0.0.0
+    ns_param    port            8000
+}
