1306e9a15122d5591694d1b0a3c5125e96c2f658
Gustaf Neumann <neumann@wu-wien.ac.at>
2021-07-20 12:57:35 +0200

    Added a somple section for using the letsencrypt NaviServer module

--- openacs-config.tcl
+++ openacs-config.tcl
@@ -928,6 +928,32 @@ ns_section ns/module/nsstats {
     ns_param bglocks  {oacs:sched_procs}
 }
 
+
+#
+# Sample letsencrypt configuration.
+#
+# To use this, it is necessary to install the NaviServer letsencrypt
+# module first, uncomment the two "ns_param" lines, and provide your
+# desired domain names.
+#
+ns_section "ns/server/${server}/modules" {
+    #ns_param letsencrypt tcl
+}
+ns_section ns/server/${server}/module/letsencrypt {
+
+    # Provide one or more domain names (latter for multi-domain SAN
+    # certificates). These values are a default in case the domains
+    # are not provided by other means (e.g. "letsencrypt.tcl").  In
+    # case multiple NaviServer virtual hosts are in used, this
+    # definition must be on the ${server}, which is used for
+    # obtaining updates (e.g. main site) although it retrieves a
+    # certificate for many subsites.
+
+    #ns_param domains { openacs.org openacs.net fisheye.openacs.org cvs.openacs.org }
+}
+
+
+
 #ns_logctl severity Debug(ns:driver) on
 #ns_logctl severity Debug(request) on
 #ns_logctl severity Debug(task) on
