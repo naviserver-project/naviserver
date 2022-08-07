589d10bb9b8ca1006dc8ffe3d92602168844c957
Gustaf Neumann <neumann@wu-wien.ac.at>
2022-08-07 20:44:08 +0200

    Upgrade License from MPL 1.1 to MPL 2
    MPL 1.1 allows anyone who received code under the terms of MPL 1.1 to
    distribute under the terms of a later version of the MPL.
    
    Reasons for upgrading the license to 2.0:
    
     - MPL 2.0 makes compliance simpler, both for the code provider and receiver.
     - MPL 2.0 provides patent protections for the provider and contributors and
       allows the entire community to protect any contributor if the contributor is sued.
     - Compatibility with Apache and GPL makes code reuse and redistribution easier
       for the provider and for the broader open source community.
    
    In essence, this means, nothing changes concerning the ability to reuse
    NaviServer in all kind of projects; double-licensing of MPL+GPL is not
    necessary anymore, since MPL 2.0 is GPL compatible.
    
    https://www.mozilla.org/en-US/MPL/2.0/Revision-FAQ/

--- nsd/fastpath.c
+++ nsd/fastpath.c
@@ -8,22 +8,10 @@
  * the License for the specific language governing rights and limitations
  * under the License.
  *
- * The Original Code is AOLserver Code and related documentation
- * distributed by AOL.
+ * The Initial Developer of the Original Code and related documentation
+ * is America Online, Inc. Portions created by AOL are Copyright (C) 1999
+ * America Online, Inc. All Rights Reserved.
  *
- * The Initial Developer of the Original Code is America Online,
- * Inc. Portions created by AOL are Copyright (C) 1999 America Online,
- * Inc. All Rights Reserved.
- *
- * Alternatively, the contents of this file may be used under the terms
- * of the GNU General Public License (the "GPL"), in which case the
- * provisions of GPL are applicable instead of those above.  If you wish
- * to allow use of your version of this file only under the terms of the
- * GPL and not to allow others to use your version of this file under the
- * License, indicate your decision by deleting the provisions above and
- * replace them with the notice and other provisions required by the GPL.
- * If you do not delete the provisions above, a recipient may use your
- * version of this file under either the License or the GPL.
  */
 
 
