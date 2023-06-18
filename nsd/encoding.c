a0d4afe408a8b8f947b54872091604d4a377377e
Gustaf Neumann <neumann@wu-wien.ac.at>
2023-06-18 20:08:37 +0200

    fixed typo

--- nsd/encoding.c
+++ nsd/encoding.c
@@ -349,6 +349,8 @@ Ns_GetFileEncoding(const char *file)
         if (hPtr != NULL) {
             const char *name = Tcl_GetHashValue(hPtr);
             encoding = Ns_GetCharsetEncoding(name);
+            Ns_Log(Debug, "Ns_GetFileEncoding for file '%s' with ext '%s' = %s %p",
+                   file, ext, name, (void*)encoding);
         }
     }
     return encoding;
@@ -689,7 +691,7 @@ LoadEncoding(const char *name)
  *
  * AddCharset, AddExtension --
  *
- *      Add extensiont to encoding mapping and charset aliases.
+ *      Add extension to encoding mapping and charset aliases.
  *
  * Results:
  *      None.
