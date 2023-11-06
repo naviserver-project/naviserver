48c541c3ac647c1f45462d3c9dd90fbfbd7f1b48
Gustaf Neumann <neumann@wu-wien.ac.at>
2023-11-06 13:47:07 +0100

    nscgi: added environment variable SCRIPT_FILENAME
    As requested on [1] the environment variable SCRIPT_FILENAME was
    added, which contains the absolute path name (file system) of the
    currently executing script. Many thanks to Danilo Raynor for the
    report and suggested change.
    
    [1] https://sourceforge.net/p/naviserver/bugs/92/

--- nscgi/nscgi.c
+++ nscgi/nscgi.c
@@ -886,6 +886,7 @@ CgiExec(Cgi *cgiPtr, Ns_Conn *conn)
      */
 
     Ns_SetUpdateSz(cgiPtr->env, "SCRIPT_NAME", 11, cgiPtr->name, TCL_INDEX_NONE);
+    Ns_SetUpdateSz(cgiPtr->env, "SCRIPT_FILENAME", 15, cgiPtr->path, -1);
     Ns_SetUpdateSz(cgiPtr->env, "REQUEST_URI", 11, Ns_ConnTarget(conn, dsPtr), TCL_INDEX_NONE);
     Ns_DStringSetLength(dsPtr, 0);
 
