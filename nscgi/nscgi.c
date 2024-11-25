0542fe9966fa9f5ed6d416ae6c4c11703c3d3649
Gustaf Neumann <neumann@wu-wien.ac.at>
2024-11-25 11:20:28 +0100

    whitespace changes

--- nscgi/nscgi.c
+++ nscgi/nscgi.c
@@ -176,9 +176,8 @@ AddCmds(Tcl_Interp *interp, const void *arg)
     const Mod *modPtr = arg;
 
     Ns_Log(Ns_LogCGIDebug, "nscgi: adding command ns_register_cgi");
-    (void)TCL_CREATEOBJCOMMAND(interp, "ns_register_cgi",
-                               NsTclRegisterCGIObjCmd, (ClientData)modPtr,
-                               NULL);
+    (void)TCL_CREATEOBJCOMMAND(interp, "ns_register_cgi", NsTclRegisterCGIObjCmd,
+                               (ClientData)modPtr, NULL);
     return TCL_OK;
 }
 
