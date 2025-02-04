b23127329d28a5d7a485f953c37f01ac1cd364e1
Gustaf Neumann <neumann@wu-wien.ac.at>
2025-02-04 10:14:33 +0100

    added usual MPL copyright notice

--- revproxy/revproxy-procs.tcl
+++ revproxy/revproxy-procs.tcl
@@ -1,4 +1,13 @@
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code and related documentation
+# is America Online, Inc. Portions created by AOL are Copyright (C) 1999
+# America Online, Inc. All Rights Reserved.
+#
+#
 # Tcl module for NaviServer to use it as a reverse proxy server.
 #
 
