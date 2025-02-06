6cf5cf9260232536be48d60efb7c65a01e4ea4ca
Gustaf Neumann <neumann@wu-wien.ac.at>
2025-02-06 11:15:36 +0100

    Added support for "-all" flag in "ns_config"
    - Added support for a new "-all" flag for "ns_config" command return
      all values from a configuration multi-set rather than just the first
      value.
    
    - Introduced helper functions GetBoolFromStringOrDefault,
      GetIntFromStringOrDefault, and ReturnAllValues to centralize type
      conversion, range checking, and default value handling for
      configuration parameters.
    
    - Add errors, when retrieving a "-bool" or "-int" value
      but multiple values are present.
    
    - Added warnings when retrieving only the first value via "ns_config"
      but multiple values are present.
    
    - Enhance the ns_config manual (ns_config.man) to document the new
      "-all" flag, which returns all values from a multi-set rather than
      only the first value. The documentation now includes detailed
      examples for merging configuration sections, updating sections with
      "-update", and retrieving multiple values.
    
    - Update regression tests in ns_config.test and test.nscfg to reflect these changes.

--- nsd/tclconf.c
+++ nsd/tclconf.c
@@ -18,7 +18,111 @@
 
 #include "nsd.h"
 
-
+static int GetBoolFromStringOrDefault(Tcl_Interp *interp, const char *value, Tcl_Obj *defObj);
+static int GetIntFromStringOrDefault(Tcl_Interp *interp, const char *value, Tcl_Obj *defObj, Tcl_WideInt minValue, Tcl_WideInt maxValue);
+static void ReturnAllValues(Tcl_Interp *interp, int all, Ns_DList *dlPtr)
+    NS_GNUC_NONNULL(1) NS_GNUC_NONNULL(3);
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * GetBoolFromDefault, GetIntFromStringOrDefault --
+ *
+ *      Helper functions for NsTclConfigObjCmd() for getting booleans or value
+ *      ranged integer values either from string values (from the
+ *      configuration) or from defaults.
+ *
+ * Results:
+ *      Tcl result.
+ *
+ * Side effects:
+ *      Setting interpreter result.
+ *
+ *----------------------------------------------------------------------
+ */
+static int
+GetBoolFromStringOrDefault(Tcl_Interp *interp, const char *value, Tcl_Obj *defObj)
+{
+    int i, result = TCL_OK;
+
+    if (value != NULL) {
+        if (Tcl_GetBoolean(interp, value, &i) != TCL_OK) {
+            if (defObj != NULL) {
+                result = Tcl_GetBooleanFromObj(interp, defObj, &i);
+            } else {
+                result = TCL_ERROR;
+            }
+        }
+    } else if (defObj != NULL) {
+        result = Tcl_GetBooleanFromObj(interp, defObj, &i);
+    }
+
+    if (result == TCL_OK) {
+        Tcl_SetObjResult(interp, Tcl_NewBooleanObj(i));
+    }
+    return result;
+}
+
+static int
+GetIntFromStringOrDefault(Tcl_Interp *interp, const char *value, Tcl_Obj *defObj, Tcl_WideInt minValue, Tcl_WideInt maxValue)
+{
+    int result = TCL_OK;
+    Tcl_WideInt v;
+
+    if (value != NULL && (Ns_StrToWideInt(value, &v) != NS_OK)) {
+        /*
+         * There is no Tcl_GetWideInt so we put same error message as
+         * Tcl_GetInt.
+         */
+        Ns_TclPrintfResult(interp, "expected integer but got \"%s\"", value);
+        result = TCL_ERROR;
+    } else if (defObj != NULL && Tcl_GetWideIntFromObj(interp, defObj, &v) != TCL_OK) {
+        result = TCL_ERROR;
+    } else if (v >= minValue && v <= maxValue) {
+        Tcl_SetObjResult(interp, Tcl_NewWideIntObj(v));
+    } else {
+        Ns_TclPrintfResult(interp, "value '%s' out of range", value != NULL ? value : Tcl_GetString(defObj));
+        result = TCL_ERROR;
+    }
+    return result;
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * ReturnAllValues --
+ *
+ *      Helper functions for NsTclConfigObjCmd() to set the interpreter result
+ *      to the first all all values of a Ns_DList structure, depending on the
+ *      "all" flag.
+ *
+ * Results:
+ *      None.
+ *
+ * Side effects:
+ *      Setting interpreter result.
+ *
+ *----------------------------------------------------------------------
+ */
+static void
+ReturnAllValues(Tcl_Interp *interp, int all, Ns_DList *dlPtr)
+{
+    NS_NONNULL_ASSERT(interp != NULL);
+    NS_NONNULL_ASSERT(dlPtr != NULL);
+
+    if (all != 0) {
+        Tcl_Obj *resultObj = Tcl_NewListObj((TCL_SIZE_T)dlPtr->size, NULL);
+        size_t   i;
+
+        for (i = 0u; i < dlPtr->size; i++) {
+            Tcl_ListObjAppendElement(interp, resultObj, Tcl_NewStringObj(dlPtr->data[i], TCL_INDEX_NONE));
+        }
+        Tcl_SetObjResult(interp, resultObj);
+    } else {
+        Tcl_SetObjResult(interp, Tcl_NewStringObj(dlPtr->data[0], TCL_INDEX_NONE));
+    }
+}
+
 /*
  *----------------------------------------------------------------------
  *
@@ -34,16 +138,16 @@
  *
  *----------------------------------------------------------------------
  */
-
 int
 NsTclConfigObjCmd(ClientData UNUSED(clientData), Tcl_Interp *interp, TCL_SIZE_T objc, Tcl_Obj *const* objv)
 {
     char       *section;
     Tcl_Obj    *defObj = NULL, *keyObj;
-    int         status, isBool = 0, isInt = 0, exact = 0, doSet = 0;
+    int         status = TCL_OK, isBool = 0, isInt = 0, exact = 0, doSet = 0, all = 0;
     Tcl_WideInt minValue = LLONG_MIN, maxValue = LLONG_MAX;
 
     Ns_ObjvSpec opts[] = {
+        {"-all",   Ns_ObjvBool,      &all,      INT2PTR(NS_TRUE)},
         {"-bool",  Ns_ObjvBool,      &isBool,   INT2PTR(NS_TRUE)},
         {"-int",   Ns_ObjvBool,      &isInt,    INT2PTR(NS_TRUE)},
         {"-min",   Ns_ObjvWideInt,   &minValue, NULL},
@@ -64,87 +168,76 @@ NsTclConfigObjCmd(ClientData UNUSED(clientData), Tcl_Interp *interp, TCL_SIZE_T 
 
     } else {
         const char *value;
-        bool        done = NS_FALSE;
         char       *keyString;
         TCL_SIZE_T  keyLength;
+        Ns_DList    dl, *dlPtr = &dl;
+        size_t      count;
+        Ns_Set     *set = Ns_ConfigGetSection(section);
 
         if (minValue > LLONG_MIN || maxValue < LLONG_MAX) {
             isInt = 1;
         }
 
+        Ns_DListInit(dlPtr);
         keyString = Tcl_GetStringFromObj(keyObj, &keyLength);
-        value = (exact != 0) ?
-            Ns_ConfigGetValueExact(section, keyString) :
-            Ns_ConfigGetValue(section, keyString);
 
-        /*
-         * Handle type checking of config value.
-         */
+        count = likely(set != NULL)
+            ? NsSetGetCmpDListAppend(set, keyString, NS_TRUE, exact == 0 ? strcmp : strcasecmp, dlPtr)
+            : 0u;
 
-        status = TCL_OK;
-
-        if (isBool != 0) {
-            if (value != NULL) {
-                int i;
-                status = Tcl_GetBoolean(interp, value, &i);
-                if (status == TCL_OK) {
-                    Tcl_SetObjResult(interp, Tcl_NewBooleanObj(i));
-                    done = NS_TRUE;
-                }
-            }
-        } else if (isInt != 0) {
-            if (value != NULL) {
-                Tcl_WideInt v;
-
-                /*
-                 * There is no Tcl_GetWideInt so we put same error message as
-                 * Tcl_GetInt.
-                 */
-
-                if (unlikely(Ns_StrToWideInt(value, &v) != NS_OK)) {
-                    Ns_TclPrintfResult(interp, "expected integer but got \"%s\"", value);
-                    done = NS_TRUE;
-                    status = TCL_ERROR;
-                }  else if (v >= minValue && v <= maxValue) {
-                    Tcl_SetObjResult(interp, Tcl_NewWideIntObj(v));
-                    done = NS_TRUE;
-                } else {
-                    Ns_TclPrintfResult(interp, "value '%s' out of range", value);
-                    status = TCL_ERROR;
-                }
-            }
-        } else if (value != NULL) {
-            Tcl_SetObjResult(interp, Tcl_NewStringObj(value, TCL_INDEX_NONE));
-            done = NS_TRUE;
-        }
-
-        /*
-         * Handle default value.
-         */
-
-        if (!done && defObj != NULL) {
-            status = TCL_OK;
+        if (count == 1) {
+            /*
+             * We got a single value
+             */
+            value = dlPtr->data[0];
 
             if (isBool != 0) {
-                int i;
-                if (unlikely(Tcl_GetBooleanFromObj(interp, defObj, &i) != TCL_OK)) {
-                    status = TCL_ERROR;
-                } else {
-                    defObj = Tcl_NewIntObj(i);
-                }
-            } else if (isInt != 0) {
-                Tcl_WideInt v;
+                status = GetBoolFromStringOrDefault(interp, value, defObj);
 
-                if (Tcl_GetWideIntFromObj(interp, defObj, &v) != TCL_OK) {
-                    status = TCL_ERROR;
-                } else if (v < minValue || v > maxValue) {
-                    Ns_TclPrintfResult(interp, "value '%s' out of range", Tcl_GetString(defObj));
-                    status = TCL_ERROR;
-                }
+            } else if (isInt != 0) {
+                status = GetIntFromStringOrDefault(interp, value, defObj, minValue, maxValue);
+
+            } else {
+                ReturnAllValues(interp, all, dlPtr);
+            }
+
+        } else if (count > 1) {
+            /*
+             * We got multiple values
+             */
+            if (isBool != 0) {
+                Ns_TclPrintfResult(interp, "ns_config: -bool flag implies a single value, but got %ld values", count);
+                status = TCL_ERROR;
+
+            } else if (isInt != 0) {
+                Ns_TclPrintfResult(interp, "ns_config: -int flag implies a single value, but got %ld values", count);
+                status = TCL_ERROR;
+
+            } else if (all) {
+                ReturnAllValues(interp, all, dlPtr);
+
+            } else {
+                Ns_Log(Warning, "ns_config: returns the first of %ld values (section '%s' key '%s')",
+                       count, section, keyString);
+                Tcl_SetObjResult(interp, Tcl_NewStringObj(dlPtr->data[0], TCL_INDEX_NONE));
+            }
+
+        } else if (defObj != NULL /* && count == 0 */) {
+            /*
+             * Found no values, use the default value.
+             */
+
+            if (isBool != 0) {
+                status = GetBoolFromStringOrDefault(interp, NULL, defObj);
+
+            } else if (isInt != 0) {
+                status = GetIntFromStringOrDefault(interp, NULL, defObj, minValue, maxValue);
             }
 
             if (status == TCL_OK && doSet != 0 && !nsconf.state.started) {
-                Ns_Set *set = Ns_ConfigCreateSection(section);
+                if (set == NULL) {
+                    set = Ns_ConfigCreateSection(section);
+                }
 
                 /* make setting queryable */
                 if (set != NULL) {
@@ -158,6 +251,7 @@ NsTclConfigObjCmd(ClientData UNUSED(clientData), Tcl_Interp *interp, TCL_SIZE_T 
                 Tcl_SetObjResult(interp, defObj);
             }
         }
+        Ns_DListFree(dlPtr);
     }
     /*
      * Either TCL_OK and "" result because matching config not found, or
