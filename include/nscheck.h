3f817a813c66e612777c96718b79c2293b5e0dd9
Gustaf Neumann <neumann@wu-wien.ac.at>
2025-10-17 10:32:09 +0200

    check(macros): switch NS_GNUC_NONNULL() to C99 variadic form
    - Updated macro NS_GNUC_NONNULL() to use __VA_ARGS__ instead of a fixed
      macro argument list for cleaner syntax and better usability with
      multiple parameters.
    - Keeps behavior identical under GCC >= 3.3, while improving readability
      and flexibility in C99 and later code.
    - Simplifies attribute usage such as: NS_GNUC_NONNULL(1, 2, 4)
      instead of multiple single-argument declarations.
    
    Since we require c99 since many years, this is a safe extension and
    aligns the macro with newer style (C99) variadic preprocessor conventions.

--- include/nscheck.h
+++ include/nscheck.h
@@ -43,10 +43,10 @@
 
 
 #if __GNUC_PREREQ(3,3)
-# define NS_GNUC_NONNULL(ARGS) __attribute__((__nonnull__(ARGS)))
+# define NS_GNUC_NONNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
 # define NS_GNUC_MAYALIAS __attribute__((__may_alias__))
 #else
-# define NS_GNUC_NONNULL(ARGS)
+# define NS_GNUC_NONNULL(...)
 # define NS_GNUC_MAYALIAS
 #endif
 
